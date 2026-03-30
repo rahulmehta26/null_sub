7c5ac6aabbd8d6306bcd368d009a6dcd2acc1829
rahulmehta26 <rk6658542@gmail.com>
2026-03-30 23:15:20 +0530

    add svg icons

---
+++ src/components/icons/TrendingUp.tsx
@@ -0,0 +1,22 @@
+import { cn } from "../../utils/cn";
+
+const TrendingUp = ({ className }: { className?: string }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn("icons", className)}
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 17l6 -6l4 4l8 -8" />
+            <path d="M14 7l7 0l0 7" />
+        </svg>
+    );
+};
+
+export default TrendingUp;
