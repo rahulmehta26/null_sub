56ec51c58a8dca7c54a6a443deb30f30707cb3d4
rahulmehta26 <rk6658542@gmail.com>
2026-04-01 22:14:32 +0530

    add app routing with lazy-loaded pages and suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { lazy, Suspense } from "react"
+import Layout from "./components/layouts/Layout"
+import { Navigate, Route, Routes } from "react-router-dom"
+import PageLoader from "./components/ui/PageLoader"
+import Home from "./pages/home/Home"
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Subscriptions = lazy(() => import("./pages/Subscriptions"));
+const Analytics = lazy(() => import("./pages/Analytics"));
+const AddSubscription = lazy(() => import("./pages/AddSubscription"));
+
 const App = () => {
   return (
-    <div
-      className=' p-10 space-x-10 '
-    >
-
-    </div>
+    <Layout>
+      <Suspense
+        fallback={<PageLoader />}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/subscriptions" element={<Subscriptions />} />
+          <Route path="/add" element={<AddSubscription />} />
+          <Route path="/edit/:id" element={<AddSubscription />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
+    </Layout>
   )
 }
 
