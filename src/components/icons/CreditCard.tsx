c4af5ed6a692f6a3411edba6ffc527661f278785
rahulmehta26 <rk6658542@gmail.com>
2026-04-01 22:38:46 +0530

    add svg icon components

--- src/components/icons/CreditCard.tsx
+++ src/components/icons/CreditCard.tsx
@@ -1,6 +1,9 @@
 import { cn } from "../../utils/cn";
 
-const BarChart = ({ className, ...props }: { className?: string }) => {
+const CreditCard = ({ className, ...props }: {
+    className?: string
+
+}) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -14,14 +17,12 @@ const BarChart = ({ className, ...props }: { className?: string }) => {
             {...props}
         >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M3 3v18h18" />
-            <path d="M20 18v3" />
-            <path d="M16 16v5" />
-            <path d="M12 13v8" />
-            <path d="M8 16v5" />
-            <path d="M3 11c6 0 5 -5 9 -5s3 5 9 5" />
+            <path d="M3 8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3l0 -8" />
+            <path d="M3 10l18 0" />
+            <path d="M7 15l.01 0" />
+            <path d="M11 15l2 0" />
         </svg>
     );
 };
 
-export default BarChart;
+export default CreditCard;
