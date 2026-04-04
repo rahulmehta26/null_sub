08520515c4bdd2d4ef16aac745de04f454bc6daa
rahulmehta26 <rk6658542@gmail.com>
2026-04-05 00:24:13 +0530

    replaced home with landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense } from "react"
 import Layout from "./components/layouts/Layout"
 import { Navigate, Route, Routes } from "react-router-dom"
 import PageLoader from "./components/ui/PageLoader"
-import Home from "./pages/home/Home"
+import LandingPage from "./pages/landing/LandingPage"
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Subscriptions = lazy(() => import("./pages/Subscriptions"));
@@ -16,7 +16,7 @@ const App = () => {
         fallback={<PageLoader />}
       >
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/subscriptions" element={<Subscriptions />} />
           <Route path="/add" element={<AddSubscription />} />
