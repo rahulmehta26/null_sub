34a15eead9779ba32717d9d577eeb11d56056543
rahulmehta26 <rk6658542@gmail.com>
2026-04-04 23:46:33 +0530

    add svg icon components

---
+++ src/components/icons/ArrowRight.tsx
@@ -0,0 +1,24 @@
+import { cn } from "../../utils/cn";
+
+const ArrowRight = ({ className, ...props }: { className?: string }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn("icons", className)}
+            {...props}
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M5 12l14 0" />
+            <path d="M15 16l4 -4" />
+            <path d="M15 8l4 4" />
+        </svg>
+    );
+};
+
+export default ArrowRight;
